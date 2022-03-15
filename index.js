437a94ea52de4f0918422211ba3007ec1481cae7
Jeong Chong In <chongin12@naver.com>
2022-03-15 21:43:59 +0900

    body-parser 설치, 회원가입 기능

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const { User } = require('./models/User')
+const bodyParser = require('body-parser')
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}))
+
+//application/json
+app.use(bodyParser.json())
+
 const mongoose = require('mongoose')
 mongoose.connect("mongodb+srv://chongin12:abcd1234@boilerplate.pjgbv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
   .then(() => console.log("MongoDB Connected..."))
@@ -9,6 +18,18 @@ mongoose.connect("mongodb+srv://chongin12:abcd1234@boilerplate.pjgbv.mongodb.net
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+  // 회원가입할 때 필요한 정보들을 client에서 가져오면 DB에 넣어준다.
+  const user = new User(req.body)
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({ 
+      success: true
+    })
+  })
+  
 })
 
 app.listen(port, () => {
